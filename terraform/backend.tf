4282a745d73d3fe98d105084c19e27b865133b1d
terraform-fmt <noreply@corise.com>
2023-07-08 23:51:10 +0000

    fixup: terraform fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-flaskbb-mziener" 
+    bucket = "terraform-state-flaskbb-mziener"
     key    = "core/terraform.tfstate"
     region = "eu-north-1"
   }
